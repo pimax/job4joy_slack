29c4b27ee6a5337818e3d29e02ca99eae20a9cae
pimax <m.pinyugin@gmail.com>
2016-04-23 22:39:42 +0300

    fixed

--- index.php
+++ index.php
@@ -4,6 +4,12 @@ require 'vendor/autoload.php';
 
 use PhpSlackBot\Bot;
 
+
+$config = []; // config
+if (file_exists(__DIR__.'/config.php')) {
+    $config = include __DIR__.'/config.php';
+}
+
 class MyCommand extends \PhpSlackBot\Command\BaseCommand
 {
     protected function configure()
@@ -13,7 +19,7 @@ class MyCommand extends \PhpSlackBot\Command\BaseCommand
 
     protected function execute($message, $context)
     {
-        $this->send($this->getCurrentChannel(), null, 'Hello! I can help you with IT projects.', [
+        $res = $this->postMessage($this->getCurrentChannel(), null, 'Hello! I can help you with IT projects.', [
             /*[
                 'fallback' => 'all',
                 'color' => '#36a64f',
@@ -25,12 +31,18 @@ class MyCommand extends \PhpSlackBot\Command\BaseCommand
                 'text' => 'Test attachments'
             ]
         ]);
+
+        echo '<pre>', print_r($res, true), '</pre>';
     }
 
 
-    protected function send($channel, $username, $message, $attachments = [])
+    protected function postMessage($channel, $username, $message, $attachments = [])
     {
+        global $config;
+
         $response = array(
+            'token' => $config['token'],
+            'as_user' => true,
             'id' => time(),
             'type' => 'message',
             'channel' => $channel,
@@ -41,16 +53,27 @@ class MyCommand extends \PhpSlackBot\Command\BaseCommand
             $response['attachments'] = $attachments;
         }
 
-        echo '<pre>', print_r($response), '</pre>';
+        $url = 'https://slack.com/api/chat.postMessage';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url.'?'.http_build_query($response));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $body = curl_exec($ch);
+        if ($body === false) {
+            throw new \Exception('Error when requesting '.$url.' '.curl_error($ch));
+        }
+        curl_close($ch);
+        $response = json_decode($body, true);
+        if (is_null($response)) {
+            throw new \Exception('Error when decoding body ('.$body.').');
+        }
+        if (isset($response['error'])) {
+            throw new \Exception($response['error']);
+        }
 
-        $this->getClient()->send(json_encode($response));
+
+        return $response;
     }
 
-}
-
-$config = []; // config
-if (file_exists(__DIR__.'/config.php')) {
-    $config = include __DIR__.'/config.php';
 }
 
 $bot = new Bot();
