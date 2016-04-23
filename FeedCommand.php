7a77ca171ff88b49021e5b3f357ba5ca4e1ec9d3
pimax <m.pinyugin@gmail.com>
2016-04-23 23:25:18 +0300

    fixed

--- FeedCommand.php
+++ FeedCommand.php
@@ -20,18 +20,19 @@ abstract class FeedCommand extends AdvancedCommand
             $items = array_reverse($feed->getItems());
             if (count($items))
             {
-                foreach ($items as $itm)
-                {
+                foreach ($items as $itm) {
                     $url = $itm->getUrl();
                     $message = substr(strip_tags($itm->getContent()), 0, 150);
 
-                    $this->postMessage($token, $this->getCurrentChannel(), null, '', [
-                        [
-                            'title' => $itm->getTitle(),
-                            'title_link' => $url,
-                            'text' => $message
-                        ]
-                    ]);
+                    if ($message) {
+                        $this->postMessage($token, $this->getCurrentChannel(), null, '', [
+                            [
+                                'title' => $itm->getTitle(),
+                                'title_link' => $url,
+                                'text' => $message
+                            ]
+                        ]);
+                    }
                 }
             } else {
                 $this->getClient()->send($this->getCurrentChannel(), null, 'Not found a new projects.');
