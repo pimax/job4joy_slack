ab962a0ded90f8e4a9bc2aa4a41825fb31fe8053
pimax <m.pinyugin@gmail.com>
2016-05-14 22:55:53 +0300

    fixed

--- oauth/index.php
+++ oauth/index.php
@@ -28,7 +28,6 @@ if (is_null($response)) {
 if (isset($response['error'])) {
     throw new \Exception($response['error']);
 }
-file_put_contents(__DIR__. '/../installed/'.$response['team_id'].'.php', "<?php\nreturn " . var_export($params, true) . ";\n");
+file_put_contents(__DIR__. '/../installed/'.$response['team_id'].'.php', "<?php\nreturn " . var_export($response, true) . ";\n");
 header("Content-Type: text/html; charset=utf-8");
-//exec("/usr/bin/php /home/lucky/web/luckyfeed.co/public_html/apps/slack/index.php " . $response['team_id']. " > /dev/null &");
 echo 'Job4Joy App successful installed!';
